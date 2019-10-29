3170cfab0da82589ec410ebb8633553243bd7e8f
imkkdev <imkkdev@outlook.com>
2019-10-29 17:37:16 +0800

    use json file resources

--- app/src/androidTest/java/com/kk/testapp/PokeMainTest.kt
+++ app/src/androidTest/java/com/kk/testapp/PokeMainTest.kt
@@ -2,6 +2,7 @@ package com.kk.testapp
 
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
+import java.nio.charset.Charset
 
 class PokeMainTest : MockBase(PokeMainActivity::class.java) {
 
@@ -11,10 +12,16 @@ class PokeMainTest : MockBase(PokeMainActivity::class.java) {
 
     override fun dispatch(request: RecordedRequest?): MockResponse {
         return when (request?.path) {
-            "v2/pokemon" -> MockResponse().setBody(pokemon)
+            "v1/pokemon" -> MockResponse().setBody(pokemon)
+            "v2/pokemon" -> MockResponse().setBody(getJson("json/pokemon.json")) // can use json.file in androidTest
             else -> MockResponse().setResponseCode(404)
         }
     }
 
     private val pokemon = "{\"count\":1,\"next\":\"Next\",\"previous\":\"Previous\",\"result\":[\"name\":\"Poke\",\"url\":\"www.google.com\",\"type\":[\"type1\",\"type2\"]]}"
+
+    private fun getJson(path: String): String {
+        val stream = this.javaClass.classLoader.getResource(path)
+        return stream.readBytes().toString(Charset.defaultCharset())
+    }
 }
