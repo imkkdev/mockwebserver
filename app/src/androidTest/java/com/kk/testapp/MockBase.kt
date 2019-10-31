3cae5de00586da8bc1e9baaf2a2e72af6485675b
imkkdev <imkkdev@outlook.com>
2019-10-31 17:32:49 +0800

    activityTestRule add launch vararg fun

--- app/src/androidTest/java/com/kk/testapp/MockBase.kt
+++ app/src/androidTest/java/com/kk/testapp/MockBase.kt
@@ -2,6 +2,8 @@ package com.kk.testapp
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
 import okhttp3.mockwebserver.Dispatcher
@@ -13,6 +15,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.Serializable
 import java.util.concurrent.CountDownLatch
 
 @RunWith(AndroidJUnit4::class)
@@ -27,6 +30,12 @@ open class MockBase(activityClass: Class<out Activity>) : Dispatcher() {
     @Test
     open fun jump() {
         jumpImpl(null)
+    }
+
+    open fun jump(vararg params: Pair<String, Any?>?) {
+        activityTestRule.launchInternal(params)
+        val countdown = CountDownLatch(1)  //取消自动退出
+        countdown.await()
     }
 
     fun jumpImpl(intent: Intent?) {
@@ -55,5 +64,53 @@ open class MockBase(activityClass: Class<out Activity>) : Dispatcher() {
     }
 
     open val success = "{\"status\":\"00000\",\"msg\":\"操作成功\",\"errorMsg\":\"\",\"success\":true}"
+
+
+    private fun fillIntentArguments(params: Array<out Pair<String, Any?>?>) : Intent {
+        val intent = Intent()
+        params.forEach {
+            if (it == null)  return@forEach
+            val value = it.second
+            when (value) {
+                null -> intent.putExtra(it.first, null as Serializable?)
+                is Int -> intent.putExtra(it.first, value)
+                is Long -> intent.putExtra(it.first, value)
+                is CharSequence -> intent.putExtra(it.first, value)
+                is String -> intent.putExtra(it.first, value)
+                is Float -> intent.putExtra(it.first, value)
+                is Double -> intent.putExtra(it.first, value)
+                is Char -> intent.putExtra(it.first, value)
+                is Short -> intent.putExtra(it.first, value)
+                is Boolean -> intent.putExtra(it.first, value)
+                is Serializable -> intent.putExtra(it.first, value)
+                is Bundle -> intent.putExtra(it.first, value)
+                is Parcelable -> intent.putExtra(it.first, value)
+                is Array<*> -> when {
+                    value.isArrayOf<CharSequence>() -> intent.putExtra(it.first, value)
+                    value.isArrayOf<String>() -> intent.putExtra(it.first, value)
+                    value.isArrayOf<Parcelable>() -> intent.putExtra(it.first, value)
+                    else -> throw Exception("Intent extra ${it.first} has wrong type ${value.javaClass.name}")
+                }
+                is IntArray -> intent.putExtra(it.first, value)
+                is LongArray -> intent.putExtra(it.first, value)
+                is FloatArray -> intent.putExtra(it.first, value)
+                is DoubleArray -> intent.putExtra(it.first, value)
+                is CharArray -> intent.putExtra(it.first, value)
+                is ShortArray -> intent.putExtra(it.first, value)
+                is BooleanArray -> intent.putExtra(it.first, value)
+                else -> throw Exception("Intent extra ${it.first} has wrong type ${value.javaClass.name}")
+            }
+            return@forEach
+        }
+        return intent
+    }
+
+    open fun <T : Activity> ActivityTestRule<T>.launch(vararg params: Pair<String, Any?>?) {
+        launchActivity(fillIntentArguments(params))
+    }
+
+    private fun <T : Activity> ActivityTestRule<T>.launchInternal(params: Array<out Pair<String, Any?>?>) {
+        launchActivity(fillIntentArguments(params))
+    }
 
 }
--- app/src/androidTest/java/com/kk/testapp/PokeDetailTest.kt
+++ app/src/androidTest/java/com/kk/testapp/PokeDetailTest.kt
@@ -1,20 +1,34 @@
 package com.kk.testapp
 
-import android.content.Intent
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.RecordedRequest
+import org.junit.Test
+import java.util.concurrent.CountDownLatch
 
 class PokeDetailTest : MockBase(PokeDetailActivity::class.java) {
 
-    override fun jump() {
-        val intent = Intent()
-        intent.putExtra("name", "poke")
-        jumpImpl(intent)
+    @Test
+    fun jump1() {
+        activityTestRule.launch(
+            "name" to "poke",
+            "param" to 1
+        )
+        val countdown = CountDownLatch(1)  //取消自动退出
+        countdown.await()
+    }
+
+    @Test
+    fun jump2() {
+        jump(
+            "name" to "poke",
+            "param" to 1
+        )
     }
 
     override fun dispatch(request: RecordedRequest?): MockResponse {
         return MockResponse().setBody(body)
     }
 
-    private val body = "{\"name\":\"Poke\",\"url\":\"www.google.com\",\"type\":[\"type1\",\"type2\"]}"
+    private val body =
+        "{\"name\":\"Poke\",\"url\":\"www.google.com\",\"type\":[\"type1\",\"type2\"]}"
 }
